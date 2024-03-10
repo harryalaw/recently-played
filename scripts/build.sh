d608d47085027b334e3912060a1fdbe25e6cddc9
harryalaw <harryalaw@gmail.com>
2024-03-10 20:06:18 +0000

    Make the build work with cgo

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-GOARCH=arm64 GOOS=linux go build -tags lambda.norpc -o bootstrap -ldflags="-s -w" cmd/functions/recently-played.go
+GOARCH=arm64 GOOS=linux go build -tags lambda.norpc -o bootstrap cmd/functions/recently-played.go
 zip -jrm build/recently-played.zip bootstrap
